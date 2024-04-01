db20cdc457c55f6f87d8d5301a6ed2092b913305
Dinolisk <jhose_tito@hotmail.com>
2024-04-01 13:53:50 +0200

    Layout changes

--- src/components/Bid.jsx/Bid.jsx
+++ src/components/Bid.jsx/Bid.jsx
@@ -1,9 +1,29 @@
+import React from 'react';
+import "./Bid.css"; 
+
+
 const Bid = () => {
   return (
-    <div>
-      <h2>Place a bid</h2>
+    <div className="place-bid-container">
+      <h2>Lägg ny bud</h2>
+      <form>
+        <label htmlFor="bidValue" className="place-bid-label">
+          Bud summa:
+        </label>
+        <input
+          type="number"
+          id="bidValue"
+          className="place-bid-input"
+          placeholder="Ange budbelopp"
+          required
+        />
+        <button type="submit" className="place-bid-button">
+          Lägg bud
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Bid
+export default Bid;
+
